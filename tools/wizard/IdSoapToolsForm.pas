ashumkin/IndySoap

commit 9e27994683f76f32f57159be1ca9c5389dcf9060

improve: IdSoapTools: make it console application
Detach console when run in GUI (without "-g" option).
As a consequence print error message to console when run with "-g"
option.

tools/wizard/IdSoapToolsForm.pas
@@ -385,32 +385,36 @@ begin
   Timer1.Enabled := false;
   if not (bmFile in varIdSoapToolsCmdLine.BatchMode) then
     Exit;
-  if bmFileNotFound in varIdSoapToolsCmdLine.BatchMode then
-  begin
-    if not (bmBatch in varIdSoapToolsCmdLine.BatchMode) then
-      MessageDlg(Format('File %s not found', [varIdSoapToolsCmdLine._File]), mtError, [mbOK], 0)
+  try
+    if bmFileNotFound in varIdSoapToolsCmdLine.BatchMode then
+      raise EFileNotFoundException.CreateFmt('File %s not found', [varIdSoapToolsCmdLine._File])
     else
     begin
-      ExitCode := 1;
-      Close;
+      FFileName := varIdSoapToolsCmdLine._File;
+      memo1.Lines.Clear;
+      memo1.Lines.LoadFromFile(FFileName);
+      Caption := 'IndySoap Tools - '+FFileName;
+      SetCurrentDir(ExtractFilePath(FFileName));
+      DeleteMRU;
+      memo1.Modified := False;
+      if not (bmBatch in varIdSoapToolsCmdLine.BatchMode) then
+        memo1.SetFocus
+      else
+        Execute(FFileName);
     end;
-  end
-  else
-  begin
-    FFileName := varIdSoapToolsCmdLine._File;
-    memo1.Lines.Clear;
-    memo1.Lines.LoadFromFile(FFileName);
-    Caption := 'IndySoap Tools - '+FFileName;
-    SetCurrentDir(ExtractFilePath(FFileName));
-    DeleteMRU;
-    memo1.Modified := False;
-    if not (bmBatch in varIdSoapToolsCmdLine.BatchMode) then
-      memo1.SetFocus
-    else
-    begin
-      Execute(FFileName);
-      Close;
-    end;
+  except
+    on E: Exception do
+      if not (bmBatch in varIdSoapToolsCmdLine.BatchMode) then
+        raise
+      else
+      begin
+        if E is EFileNotFoundException then
+          ExitCode := 1
+        else
+          ExitCode := 2;
+        Writeln(E.Message);
+        Close;
+      end;
   end;
 end;
 

source/IdSoapDebug.pas
@@ -303,7 +303,7 @@ begin
   {$IFDEF OBJECT_TRACKING}
   {$IFNDEF USE_ADV}
   assert(assigned(gObjectList), ASSERT_LOCATION+': Attempt to finalize Object Tracking before it has been initialised');
-  if not IsLibrary and not GIdSoapSuppressLeakDialog and (gObjectList.TotalCount > 0) then
+  if not IsLibrary and not IsConsole and not GIdSoapSuppressLeakDialog and (gObjectList.TotalCount > 0) then
     begin
     SystemMessage(RS_ERR_DEBUG_LEAKING_OBJECTS, gObjectList.DescribeLiveObjects);
     end;

tools/wizard/IdSoapTools.dpr
@@ -20,8 +20,10 @@ Version History:
 
 program IdSoapTools;
 
+{$APPTYPE CONSOLE}
 uses
   Forms,
+  Windows,
   IdSoapToolsCmdLine in 'IdSoapToolsCmdLine.pas',
   IdSoapToolsForm in 'IdSoapToolsForm.pas' {IndySoapToolsForm},
   IdSoapAbout in 'IdSoapAbout.pas' {IndySoapToolsAbout},
@@ -32,7 +34,9 @@ uses
 begin
   Application.Initialize;
   if bmBatch in varIdSoapToolsCmdLine.BatchMode then
-    Application.ShowMainForm := False;
+    Application.ShowMainForm := False
+  else
+    FreeConsole;
   Application.CreateForm(TIndySoapToolsForm, IndySoapToolsForm);
   Application.Run;
 end.

tools/wizard/IdSoapTools.dproj
@@ -81,6 +81,7 @@
         <DCC_Define>DEBUG;$(DCC_Define)</DCC_Define>
     </PropertyGroup>
     <PropertyGroup Condition="'$(Cfg_2_Win32)'!=''">
+        <DCC_ConsoleTarget>true</DCC_ConsoleTarget>
         <DCC_Alignment>2</DCC_Alignment>
         <DCC_PentiumSafeDivide>true</DCC_PentiumSafeDivide>
         <DCC_RunTimeTypeInfo>true</DCC_RunTimeTypeInfo>

commit 2c408f45d4ed9904fdb5f8812c066def43976cca

fixed: IdSoapTools "hangs" when "-g" option is set
It "hangs" when no errors occured. Because it just does not exit after
.Execute.

Error introduced in previous commit 9e27994 (improve: IdSoapTools: make
it console application, 2015-03-19)

tools/wizard/IdSoapToolsForm.pas
@@ -400,7 +400,10 @@ begin
       if not (bmBatch in varIdSoapToolsCmdLine.BatchMode) then
         memo1.SetFocus
       else
+      begin
         Execute(FFileName);
+        Close;
+      end;
     end;
   except
     on E: Exception do